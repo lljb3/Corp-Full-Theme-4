94b085bab836a501310f0edab31ad5ffbf438405
lljb3 <lljb3@lljb3.com>
2019-01-22 17:27:09 -0600

    Added Classic Editor and removed LayerSlider

--- install/installer.php
+++ install/installer.php
@@ -35,21 +35,16 @@
 				'required'  => true,
 			),
 			array(
+				'name'		=> 'Classic Editor',
+				'slug'		=> 'classic-editor',
+				'required'  => true,
+			),
+			array(
 				'name'               => 'AnimateWP', // The plugin name.
 				'slug'               => 'animatewp', // The plugin slug (typically the folder name).
 				'source'             => get_template_directory_uri() . '/install/installer/plugins/animatewp.zip', // The plugin source.
 				'required'           => true, // If false, the plugin is only 'recommended' instead of required.
 				'version'            => '1.1.5', // E.g. 1.0.0. If set, the active plugin must be this version or higher.
-				'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
-				'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-				'external_url'       => '', // If set, overrides default API URL and points to an external URL.
-			),
-			array(
-				'name'               => 'LayerSlider', // The plugin name.
-				'slug'               => 'LayerSlider', // The plugin slug (typically the folder name).
-				'source'             => get_template_directory_uri() . '/install/installer/plugins/layerslider.zip', // The plugin source.
-				'required'           => false, // If false, the plugin is only 'recommended' instead of required.
-				'version'            => '6.7.6', // E.g. 1.0.0. If set, the active plugin must be this version or higher.
 				'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
 				'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
 				'external_url'       => '', // If set, overrides default API URL and points to an external URL.
