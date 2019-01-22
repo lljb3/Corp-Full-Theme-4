4b36ee45df9b5a622000737f438423de686fa44f
lljb3 <lljb3@lljb3.com>
2019-01-22 17:29:39 -0600

    Removed ReactJS for now

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,31 +14,4 @@ class App extends React.Component {
         const pages = await pagesRes.json();
         return { page, posts, pages };
     }
-
-    render() {
-        const pages = this.props.pages.map((page, index) => {
-            return (
-                <ul key={index}>
-                    <li>
-                        <Link
-                            as={`/page/${page.slug}`}
-                            href={`/post?slug=${page.slug}&apiRoute=page`}
-                        >
-                            <a>{page.title.rendered}</a>
-                        </Link>
-                    </li>
-                </ul>
-            )
-        })
-        return (
-            {pages}
-        )
-    }
 }
-
-const element = <App />
-
-ReactDOM.render(
-    element,
-    document.getElementById('app')
-);
--- includes/scripts.php
+++ includes/scripts.php
@@ -16,7 +16,7 @@
 			wp_enqueue_script( 'reactdomjs', 'https://unpkg.com/react-dom@16/umd/react-dom.production.min.js', array('jquery'), null, true );
 
 			/* React Component JS */
-			wp_enqueue_script( 'reactcomjs', $app_base . '/app.js', array('jquery'), null, true );
+			//wp_enqueue_script( 'reactcomjs', $app_base . '/app.js', array('jquery'), null, true );
 
 			/* Smooth Scroll JS */
 			wp_enqueue_script( 'smoothscrolljs', $app_base . '/lib/smooth-scroll.polyfills.js', array('jquery'), null, true );
